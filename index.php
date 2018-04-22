b3e1cf9b5031f73d17da638c5ed01199bff89468
Marcos <marcos-vdm@hotmail.com>
2018-04-22 18:47:35 -0300

    first commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "put your code here";
+        echo "put your code here!";
         ?>
     </body>
 </html>
